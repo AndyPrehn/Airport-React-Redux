790a398dff33c3d61c2e9b0b97f4b3465c64c9b1
AndyPrehn <133592836+AndyPrehn@users.noreply.github.com>
2023-11-21 20:02:14 -0600

    added index, app, and table. not quite working yet

--- src/components/Aero Table/AeroTable.jsx
+++ src/components/Aero Table/AeroTable.jsx
@@ -1,1 +1,27 @@
 import { useSelector } from 'react-redux';
+
+export default function AeroTable() {
+    const airlines = useSelector(store => store.airlines);
+
+    return (
+        <div id="airline-table" >
+            <h3>Airplanes in the Airport</h3>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Airlines</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {airlines.map((plane) => 
+                    <tr key={plane.airplaneNumber}>
+                        <td>{plane.airplaneName}</td>
+                        <td>{plane.airplaneNumber}</td>
+                    </tr>
+                    )}
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
+import AeroForm from '../AeroForm/AeroForm';
+import AeroTable from '../AeroTable/AeroTable';
+
 
 function App() {
 
   return (
     <div>
       <h1>Redux Airport</h1>
-      <input placeholder='Airline Name' />
+      <AeroForm />
+      <AeroTable />
       <button>Add Airline</button>
       <table>{/* Airlines should be listed here */}</table>
     </div>
